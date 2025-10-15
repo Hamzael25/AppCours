5752cff0bf97b826af77e933bc4e379a3dc8f999
Hamza EL OUARDI <hamzaelouardi@MacBook-Pro-de-Muller.local>
2025-10-15 11:08:25 +0200

    feat Bachir: Table des chapitres cree

--- server/database.js
+++ server/database.js
@@ -14,9 +14,15 @@ export async function creerBaseDeDonnees() {
     CREATE TABLE IF NOT EXISTS T_MATIERE (
       ID_MATIERE INTEGER PRIMARY KEY AUTOINCREMENT,
       NOM_MATIERE TEXT NOT NULL
-		`);
+		
 		
 		// en attente table chapitre + relation avec matiere
+		CREATE TABLE IF NOT EXISTS T_CHAPITRE (
+      ID_CHAPITRE INTEGER PRIMARY KEY AUTOINCREMENT,
+      NOM_CHAPITRE TEXT NOT NULL,
+      CHEMIN_FICHIER TEXT,
+      ID_MATIERE INTEGER NOT NULL
+    `);
 
 		  console.log("✅ Base de données initialisée avec succès !");
   return db;
